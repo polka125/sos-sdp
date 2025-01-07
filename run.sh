e8d752a66d76f5aee7697d315c3cf79977317b36
Sergei, NOVOZHILOV <snovozhilov@connect.ust.hk>
2025-01-08 07:03:57 +0800

    update run script

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 PLATFORM=${PLATFORM:-"linux/amd64"}
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-docker run --platform $PLATFORM \
+docker run --rm --platform $PLATFORM \
     -v "$(pwd)":/app \
     sos-sdp "$@"
